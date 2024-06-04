c02a628c7ad884be615c1986cc308c7aaff416ac
Andrae Vang <andrae.vang@gmail.com>
2024-06-03 21:59:40 -0700

    completed exercise 1 & 2

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,65 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            // Exercise 1
+
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            Console.WriteLine("Please pick a number between 1 - 1000");
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+
+            {
+                Console.WriteLine($"{favNumber} too low!");
+            }
+
+            else if (userInput > favNumber)
+
+            {
+                Console.WriteLine($"{favNumber} too high");
+            }
+
+            else
+
+            {
+                Console.WriteLine("You got it!");
+            }
+
+
+            //Exercise 2
+
+            Console.WriteLine("What is your favorite school subject?");
+
+            var favSubject = Console.ReadLine();
+
+            switch (favSubject)
+
+            {
+                case "English":
+                    Console.WriteLine("absolutely not");
+                    break;
+                case "History":
+                    Console.WriteLine("Power nap time");
+                    break;
+                case "Math":
+                    Console.WriteLine("Possible, let's try more");
+                    break;
+                case "Science":
+                    Console.WriteLine("absolutely not");
+                    break;
+                case "Political Science":
+                    Console.WriteLine("Snake of the world");
+                    break;
+                default:
+                    Console.WriteLine("Have not taken that class yet");
+                    break;
+            }
+
+
         }
     }
 }
